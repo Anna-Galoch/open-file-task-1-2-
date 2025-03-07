ec17dae640b51ae3586e260ae742bfcff555eb92
Anna Galochkina <galochkina_anya@mail.ru>
2025-03-07 20:32:07 +0300

    updated open_file.py

--- open_file.py
+++ open_file.py
@@ -1,37 +1,42 @@
-# Задача 1
 from pprint import pprint
-with open('recipes.txt', encoding='utf-8') as file:
-    lines = file.read().splitlines()
-proccessed_lines = []
-cook_book = {}
-current_line = 0
 
-while current_line < len(lines):
-    dish_name = lines[current_line].strip()
-    ingredients_qty = int(lines[current_line + 1].strip())
-    ingredients = []
-    current_line += 2
+# Задание 1
 
-    for ingredient_ind in range(current_line, current_line + ingredients_qty):
-        ingredient_info = lines[ingredient_ind].strip().split(' | ')
-        ingredient_name = ingredient_info[0]
-        ingredient_quantity = int(ingredient_info[1])
-        ingredient_unit = ingredient_info[2]
+def make_cook_book(file_name):
+    with open(file_name, encoding='utf-8') as file:
+        lines = file.read().splitlines()
+
+    cook_book = {}
+    current_line = 0
+
+    while current_line < len(lines):
+        dish_name = lines[current_line].strip()
+        ingredients_qty = int(lines[current_line + 1].strip())
+        ingredients = []
+        current_line += 2
+
+        for ingredient_ind in range(current_line, current_line + ingredients_qty):
+            ingredient_info = lines[ingredient_ind].strip().split(' | ')
+            ingredient_name = ingredient_info[0]
+            ingredient_quantity = int(ingredient_info[1])
+            ingredient_unit = ingredient_info[2]
         
-        ingredient_dict = {
-            'ingredient_name': ingredient_name,
-            'quantity': ingredient_quantity,
-            'measure': ingredient_unit
-        }
+            ingredient_dict = {
+                'ingredient_name': ingredient_name,
+                'quantity': ingredient_quantity,
+                'measure': ingredient_unit
+            }
         
-        ingredients.append(ingredient_dict)
-    current_line += ingredients_qty + 1    
-    cook_book[dish_name] = ingredients
+            ingredients.append(ingredient_dict)
+        current_line += ingredients_qty + 1    
+        cook_book[dish_name] = ingredients
+    return cook_book
 
-pprint(cook_book)
+file_path = 'recipes.txt'
+cook_book = make_cook_book(file_path)
+# pprint(cook_book)
 
-
-# Задача 2
+# Задание 2
 def get_shop_list_by_dishes(dishes, person_count):
 
     shopping_list = {}
@@ -39,11 +44,15 @@ def get_shop_list_by_dishes(dishes, person_count):
     for dish in dishes:
         for ingredient in cook_book[dish]:
             key = ingredient['ingredient_name']
-            value = {}
-            value.setdefault('measure', ingredient['measure'])
-            value.setdefault('quantity', int(ingredient['quantity']) * person_count)
-            shopping_list.setdefault(key, value)
+            if key not in shopping_list:
+                value = {}
+                value.setdefault('measure', ingredient['measure'])
+                value.setdefault('quantity', int(ingredient['quantity']) * person_count)
+                shopping_list.setdefault(key, value)
+            elif key in shopping_list:
+                add = int(ingredient['quantity']) * person_count
+                shopping_list[key]['quantity'] += add
 
     return shopping_list
 
-pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+pprint(get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
